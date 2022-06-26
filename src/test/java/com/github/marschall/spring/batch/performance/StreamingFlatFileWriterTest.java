7dd51ef1acb19d7a1b7973f731c70ffeb8fcf393
Philippe Marschall <philippe.marschall@gmail.com>
2022-06-26 14:32:55 +0200

    updates

--- src/test/java/com/github/marschall/spring/batch/performance/StreamingFlatFileWriterTest.java
+++ src/test/java/com/github/marschall/spring/batch/performance/StreamingFlatFileWriterTest.java
@@ -1,29 +1,28 @@
 package com.github.marschall.spring.batch.performance;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-
-import static org.junit.Assert.assertEquals;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 
-@ContextConfiguration(classes = IntegrationTestConfiguration.class)
-public class StreamingFlatFileWriterTest extends AbstractJUnit4SpringContextTests {
-  
+@SpringJUnitConfig(IntegrationTestConfiguration.class)
+public class StreamingFlatFileWriterTest {
+
   @Autowired
   private JobLauncherTestUtils jobLauncherTestUtils;
-  
+
   @Autowired
   private FileSystem fileSystem;
 
@@ -31,11 +30,11 @@ public class StreamingFlatFileWriterTest extends AbstractJUnit4SpringContextTest
   public void test() throws Exception {
     JobParametersBuilder parametersBuilder = new JobParametersBuilder();
     JobExecution job = this.jobLauncherTestUtils.launchJob(parametersBuilder.toJobParameters());
-    
+
     assertEquals(BatchStatus.COMPLETED, job.getStatus());
-    assertEquals(IntegrationTestConfiguration.items(100), readLines());
+    assertEquals(IntegrationTestConfiguration.items(100), this.readLines());
   }
-  
+
   private List<String> readLines() throws IOException {
     return Files.lines(this.fileSystem.getPath(IntegrationTestConfiguration.OUTPUT_FILE))
         .collect(Collectors.toList());
